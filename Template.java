3d2ab9876dd403dd63226bed9add97393e7004ec
Virendra Maurya <75972251+Virendra115@users.noreply.github.com>
2022-05-14 14:59:48 +0530

    Add files via upload

---
+++ Template.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+
+public class Template {
+    static FastReader sc;
+
+    static void solve() {
+        StringBuilder res = new StringBuilder();
+        int n = sc.nextInt();
+
+    }
+
+    public static void main(String args[]) throws IOException {
+        sc = new FastReader();
+        int tt = sc.nextInt();
+        while (tt-- > 0) {
+            solve();
+        }
+    }
+
+    static <E> void print(E res) {
+        System.out.println(res);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] readintarray(int n) {
+            int res[] = new int[n];
+            for (int i = 0; i < n; i++)
+                res[i] = nextInt();
+            return res;
+        }
+
+        long[] readlongarray(int n) {
+            long res[] = new long[n];
+            for (int i = 0; i < n; i++)
+                res[i] = nextLong();
+            return res;
+        }
+    }
+
+}
