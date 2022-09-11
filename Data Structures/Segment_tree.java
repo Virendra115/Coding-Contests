5ea251d7a2fb519fc6c97a4e1fec912666c667da
Virendra Maurya <75972251+Virendra115@users.noreply.github.com>
2022-09-11 09:56:15 +0530

    Update Segment_tree.java

--- Data Structures/Segment_tree.java
+++ Data Structures/Segment_tree.java
@@ -1,9 +1,10 @@
 static class SegTree {
     long[] arr, tree;
-    int n;
+    int n,change;
 
     public SegTree(long[] arr) {
         n = arr.length;
+        change = ???????;
         this.arr = Arrays.copyOf(arr, n);
         tree = new long[4 * n];
         buildSeg(0, 0, n - 1);
@@ -17,8 +18,8 @@ static class SegTree {
     long q(int l, int r) {
         return query(0, 0, n - 1, l, r);
     }
-    void u(int value, int pos) {
-        update(0, 0, n - 1, value, pos);
+    void u(int pos, int value) {
+        update(0, 0, n - 1, pos, value);
     }
 
     void buildSeg(int ind, int low, int high) {
@@ -36,23 +37,23 @@ static class SegTree {
         if (l <= low && high <= r)
             return tree[ind];
         if (r < low || high < l)
-            return Integer.MAX_VALUE;
+            return change;
         int mid = (low + high) / 2;
         long left = query(2 * ind + 1, low, mid, l, r);
         long right = query(2 * ind + 2, mid + 1, high, l, r);
         return work(left, right);
     }
 
-    void update(int ind, int low, int high, int k, int u) {
+    void update(int ind, int low, int high, int i, int val) {
         if (low == high) {
-            tree[ind] = u;
+            tree[ind] = val;
             return;
         }
         int mid = (low + high) / 2;
-        if (k <= mid)
-            update(2 * ind + 1, low, mid, k, u);
+        if (i <= mid)
+            update(2 * ind + 1, low, mid, i, val);
         else
-            update(2 * ind + 2, mid + 1, high, k, u);
+            update(2 * ind + 2, mid + 1, high, i, val);
         tree[ind] = work(tree[2 * ind + 1], tree[2 * ind + 2]);
     }
 
