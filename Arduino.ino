a96ce092dc244ed56e6895af77603d6e1219d5fc
Virendra Maurya <75972251+Virendra115@users.noreply.github.com>
2023-03-31 09:55:19 +0530

    Create Arduino.ino

---
+++ Arduino.ino
@@ -0,0 +1,47 @@
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
+#include <DHT.h>
+
+char auth[] = "       "; 
+char ssid[] = "     ";  
+char pass[] = "     ";  
+
+#define DHTPIN 2          // Digital pin 4
+
+#define DHTTYPE DHT11     // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Blynk.virtualWrite(V5, h);  //V5 is for Humidity
+  Blynk.virtualWrite(V6, t);  //V6 is for Temperature
+}
+
+void setup()
+{
+  Serial.begin(9600); // See the connection status in Serial Monitor
+  Blynk.begin(auth, ssid, pass);
+
+  dht.begin();
+
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run(); // Initiates Blynk
+  timer.run(); // Initiates SimpleTimer
+}
